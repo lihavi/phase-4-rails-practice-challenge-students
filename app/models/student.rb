5eefa6c8fb5d3b64c3d4a0985c16b9817617eeca
vivianne <viviannjoroge91@gmail.com>
2023-03-27 11:19:49 +0300

    add validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,5 @@
 class Student < ApplicationRecord
     belongs_to :instructor
+    validates :name, presence: true
+    validates :age, presence: true, numericality: { greater_than_or_equal_to: 18 }
 end
